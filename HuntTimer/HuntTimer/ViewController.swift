ca92bdbdfb1732db4bd0e394d17f9f11da8e8c04
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-03-22 20:16:51 +0900

    init: 프로젝트 생성

---
+++ HuntTimer/HuntTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HuntTimer
+//
+//  Created by Hyojung Jang on 3/22/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
